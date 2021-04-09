e5351cc9489a3a054edd197492be319ac668bf36
zeamaria <lingardzea@gmail.com>
2021-04-09 05:18:58 +0000

    1st commit

---
+++ breedFetcher.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+
+//https://api.thecatapi.com/v1/breeds/search?q=
+
+
+// Send request API to specific address
+// as part of this goes name of breed
+// in return recieve a string json object
+// later need to handle
+// what query paramaters to pass
+// when request address recieve info about particular breed
+let breed = process.argv[2];
+let url = "https://api.thecatapi.com/v1/breeds/search?q=" + breed;
+
+request(url, function(err, res, body) {
+  if (err) {
+    console.log("Invalid URL");
+  } else {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log("Breed not found.");
+    } else {
+      console.log(data[0].description);
+    }
+  }
+});
